044f0e8ab7ae85d1f4defb34cd97418ab78da7b7
bugluo <6185763@qq.com>
2016-06-19 20:43:18 +0800

    提交新的课程

---
+++ lesson 38 并发的爬一个网站/demo/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var eventproxy = require('eventproxy');
+// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
+var app = express();
+var ep = new eventproxy();
+
+// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
+// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
+// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
+// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
+
+function getChildrenData(href){
+   superagent
+   .get('https://cnodejs.org'+href)
+   .end(function(err,resss){
+      ep.emit('topic_html', [href, resss.text]);
+   })
+}
+
+app.get('/', function (req, res) {
+	var ob = [
+	];
+	superagent
+   .get('https://cnodejs.org/')
+   .end(function(err, ress){
+   		var $ = cheerio.load(ress.text);
+         var $topic = $('.topic_title');
+
+         $topic.each(function(d,text){
+            var href = $(text).attr('href');
+            ob.push({
+               title:$(text).text().replace(/\n/ig,'').trim(),
+               link:href
+            })
+            getChildrenData(href);
+         })
+
+         ep.after('topic_html', $topic.length, function (topics) {
+            var arr = [];
+            for(var i = 0;i <  $topic.length; i++){
+               var $$ = cheerio.load(topics[i][1]);
+               // console.log(topics[i]);
+               if($$('h1').text().indexOf('503') == -1){
+                  arr.push({author:$$('.changes').find('span').eq(1).find('a').text()});
+               }
+            }
+            res.send(arr);
+         })
+
+   		
+   });
+}); 
+
+// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
+app.listen(3000, function () {
+  console.log('app is listening at port 3000');
+});
